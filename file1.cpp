41754ea8a0cabb11344818e21871b1b4d0a110ef
albukhari21 <albukhariinsankamil@gmail.com>
2025-05-23 07:55:19 +0700

    inisialisasi stack dengan null top pointer

--- file1.cpp
+++ file1.cpp
@@ -39,4 +39,13 @@ public:
         cout << "Push value: " << value << endl;
         return value;
     }
+
+    // Pop operation: Remove the topmost element from the stack
+    void pop()
+    {
+        if (isEmpty())
+        {
+            
+        }
+    }
 }
