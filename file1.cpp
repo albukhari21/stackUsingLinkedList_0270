1dd95f82aa34ba44a2c391d522b565b801562d33
albukhari21 <albukhariinsankamil@gmail.com>
2025-05-23 08:24:23 +0700

    Menambahkan opsi menu untuk melihat elemen teratas stack

--- file1.cpp
+++ file1.cpp
@@ -109,6 +109,16 @@ int main()
                 stack.pop(); // Pop the top element from the stack
             }
             else
+            {
+                cout << "Stack is empty. Cannot pop." << endl;
+            }
+            break;
+        case 3:
+            if (!stack.isEmpty())
+            {
+                stack.peek(); // Get the value of the top element
+            }
+            else
         }
     }
 }
