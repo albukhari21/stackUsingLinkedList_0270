b68dc841655d798851e4e6b1de154b03f9308097
albukhari21 <albukhariinsankamil@gmail.com>
2025-05-23 08:00:26 +0700

    Menambahkan fungsi push untuk menumpuk elemen ke stack

--- file1.cpp
+++ file1.cpp
@@ -45,7 +45,12 @@ public:
     {
         if (isEmpty())
         {
-            
+            cout << "STack is empty." << endl;
         }
+
+        Node *temp = top; // Create a temporary pointer to the top node
+        top = top->next;  // Update the top pointer to the next node
+        cout << "Popped value: " << top->data << endl;
+        delete temp;
     }
 }
